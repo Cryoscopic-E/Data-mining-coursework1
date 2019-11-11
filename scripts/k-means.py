b923c349e9ac51041db4646c993c379ea8d9a867
frsrgrrw <55896807+frsrgrrw@users.noreply.github.com>
2019-11-11 10:33:09 +0000

    k-means final

--- scripts/k-means.py
+++ scripts/k-means.py
@@ -1,108 +1,219 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import data_operations
 import constants
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
+from sklearn.model_selection import train_test_split
 
-# load and split data
-X = pd.read_csv('../data/x_train_gr_nrm_slc_rnd_smpl.csv')
+# explained variance plot
+def pca_explained_variance_plot(data,x1,x2,y1,y2):
+    pca = PCA().fit(data)
+    plt.figure()
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('Number of Components')
+    plt.ylabel('Variance (%)')
+    plt.title('German Street Sign Dataset Explained Variance')
+    plt.xlim(x1,x2)
+    plt.ylim(y1,y2)
+    plt.savefig('explained_variance.png')
+    plt.show()
+
+# silhouette score plot
+def silhouette_score_plot(data, n):
+    cluster_silhouette_score = []
+    for k in range(2, n+1):
+        kmeans = kmeans_clustering(data, k)
+        cluster_silhouette_score.append(silhouette_score(data, kmeans.labels_))
+    plt.plot(np.arange(2,n+1,1), cluster_silhouette_score)
+    plt.xticks(np.arange(2,n+1,1))
+    plt.xlabel('k (number of clusters)')
+    plt.ylabel('Silhouette score')
+    plt.show()
+
+# Cluster Evaluation
+def analyzeCluster(clusterDF, labelsDF):
+    countByCluster = pd.DataFrame(data=clusterDF['cluster'].value_counts())
+    countByCluster.reset_index(inplace=True,drop=False)
+    countByCluster.columns = ['cluster','clusterCount']
+
+    preds = pd.concat([labelsDF,clusterDF], axis=1)
+    preds.columns = ['trueLabel','cluster']
+
+    countByLabel = pd.DataFrame(data=preds.groupby('trueLabel').count())
+
+    countMostFreq = pd.DataFrame(data=preds.groupby('cluster').agg( \
+                        lambda x:x.value_counts().iloc[0]))
+    countMostFreq.reset_index(inplace=True,drop=False)
+    countMostFreq.columns = ['cluster','countMostFrequent']
+
+    accuracyDF = countMostFreq.merge(countByCluster, \
+                        left_on="cluster",right_on="cluster")
+    overallAccuracy = accuracyDF.countMostFrequent.sum()/ \
+                        accuracyDF.clusterCount.sum()
+
+    accuracyByLabel = accuracyDF.countMostFrequent/ \
+                        accuracyDF.clusterCount
+
+    return countByCluster, countByLabel, countMostFreq, \
+            accuracyDF, overallAccuracy, accuracyByLabel
+
+
+# accuracy by number of clusters
+def accuracy_by_num_clusters(X, y, n_clusters, n_init, max_iter, tol, random_state, n_jobs):
+    kMeans_inertia = \
+        pd.DataFrame(data=[],index=range(2,21),columns=['inertia'])
+    overallAccuracy_kMeansDF = \
+        pd.DataFrame(data=[],index=range(2,21),columns=['overallAccuracy'])
+
+    for n_clusters in range(2,21):
+        kmeans = KMeans(n_clusters=n_clusters, n_init=n_init, \
+                    max_iter=max_iter, tol=tol, random_state=random_state, \
+                    n_jobs=n_jobs)
+
+        cutoff = 99
+        kmeans.fit(X.loc[:,0:cutoff])
+        kMeans_inertia.loc[n_clusters] = kmeans.inertia_
+        X_train_kmeansClustered = kmeans.predict(X.loc[:,0:cutoff])
+        X_train_kmeansClustered = \
+            pd.DataFrame(data=X_train_kmeansClustered, index=X_train.index, \
+                         columns=['cluster'])
+
+        countByCluster_kMeans, countByLabel_kMeans, countMostFreq_kMeans, \
+            accuracyDF_kMeans, overallAccuracy_kMeans, accuracyByLabel_kMeans \
+            = analyzeCluster(X_train_kmeansClustered, y)
+
+        overallAccuracy_kMeansDF.loc[n_clusters] = overallAccuracy_kMeans
+
+    return overallAccuracy_kMeansDF
+
+
+# # accuracy by number of components (PCA)
+# def accuracy_by_num_components(X, y, n_clusters,n_init, max_iter, tol, random_state, n_jobs):
+#     kMeans_inertia = pd.DataFrame(data=[],index=[9, 49, 99, 199, \
+#                         299, 399, 499, 599, 699, 784],columns=['inertia'])
+#
+#     overallAccuracy_kMeansDF = pd.DataFrame(data=[],index=[9, 49, \
+#                         99, 199, 299, 399, 499, 599, 699, 784], \
+#                         columns=['overallAccuracy'])
+#
+#     for cutoffNumber in [9, 49, 99, 199, 299, 399, 499, 599, 699, 784]:
+#         kmeans = KMeans(n_clusters=n_clusters, n_init=n_init, \
+#                     max_iter=max_iter, tol=tol, random_state=random_state, \
+#                     n_jobs=n_jobs)
+#
+#         cutoff = cutoffNumber
+#         kmeans.fit(X.loc[:,0:cutoff])
+#         kMeans_inertia.loc[cutoff] = kmeans.inertia_
+#         X_kmeansClustered = kmeans.predict(X.loc[:,0:cutoff])
+#         X_kmeansClustered = pd.DataFrame(data=X_kmeansClustered, \
+#                                     index=X.index, columns=['cluster'])
+#
+#         countByCluster_kMeans, countByLabel_kMeans, countMostFreq_kMeans, \
+#             accuracyDF_kMeans, overallAccuracy_kMeans, accuracyByLabel_kMeans \
+#             = analyzeCluster(X_kmeansClustered, y)
+#
+#         overallAccuracy_kMeansDF.loc[cutoff] = overallAccuracy_kMeans
+#
+#         return overallAccuracy_kMeansDF.plot()
+
+def get_label_mapping(kmeans, correct_labels):
+    label_map = {}
+    for i in range(kmeans.n_clusters):
+        label_counter = {'Class 0':0,'Class 1':0,'Class 2':0,'Class 3':0,'Class 4':0,'Class 5':0,'Class 6':0,'Class 7':0,'Class 8':0,'Class 9':0}
+        for idx in range(len(correct_labels)):
+            if kmeans.labels_[idx] == i:
+                item = correct_labels.values[idx]
+                if item == 0:
+                    label_counter['Class 0'] += 1
+                elif item == 1:
+                    label_counter['Class 1'] += 1
+                elif item == 2:
+                    label_counter['Class 2'] += 1
+                elif item == 3:
+                    label_counter['Class 3'] += 1
+                elif item == 4:
+                    label_counter['Class 4'] += 1
+                elif item == 5:
+                    label_counter['Class 5'] += 1
+                elif item == 6:
+                    label_counter['Class 6'] += 1
+                elif item == 7:
+                    label_counter['Class 7'] += 1
+                elif item == 8:
+                    label_counter['Class 8'] += 1
+                elif item == 9:
+                    label_counter['Class 9'] += 1
+                else:
+                    print('out of range')
+        label_map[i] = label_counter
+    return label_map
+
+# workbook
+
+X = pd.read_csv('../data/x_train_gr_norm_slc_rnd_smpl.csv')
 y = pd.read_csv('../data/y_train_smpl.csv')
 y = data_operations.randomize_data(y, constants.SEED)
 
-# unsupervised, don't really need to split?
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=3)
+train_index = range(0,len(X_train))
+test_index = range(0,len(X_test))
 
-# REDUCE DIMENSIONALITY (can use either tSNE or PCA, tSNE takes much longer and need to optimise for perplexity but gives better approximation)
-# from sklearn.manifold import TSNE
-# X_train_tsne = TSNE(n_components=3, perplexity=30).fit_transform(X_train)
-from sklearn.decomposition import PCA
-X_train_pca = PCA(n_components=3).fit_transform(X_train)
+# pca variance
+pca_explained_variance_plot(X_train,0,200,0.5,1 )
 
+# set PCA for PCA at 100 components
+n_components = 100
+whiten = False
+random_state = 42
+pca = PCA(n_components=n_components, whiten=whiten, random_state=random_state)
+X_train_PCA = pca.fit_transform(X_train)
+X_train_PCA = pd.DataFrame(data=X_train_PCA, index=train_index)
 
-# UNSUPERVISED KMEANS CLUSTERING
-from sklearn.cluster import KMeans
-# More than 10 may also be okay, I am currently checking metrics to find the optimal number of clusters
-k = 10
-kmeans = KMeans(n_clusters=k,random_state=3)
-y_labels_train = kmeans.fit_(X_train_pca)
+# K-means - Accuracy as the number of clusters varies
+n_clusters = 20
+n_init = 10
+max_iter = 300
+tol = 0.0001
+random_state = 42
+n_jobs = 2
 
-# label the clusters
-y_labels_train = kmeans.labels_
+accuracy_by_clusters = accuracy_by_num_clusters(X_train_PCA,y_train,n_clusters,n_init,max_iter,tol,random_state,n_jobs)
+accuracy_by_clusters.plot().get_figure().savefig('overallAccuracyClusters.png')
 
-# get the centroids of the clusters
-centroids = kmeans.cluster_centers_
-
-
-# CLUSTER EVALUATION
-
-# method 1 gets corresponding classes, 2 & 3 provide some checks
-y_train = np.squeeze(y_train)
-
-# method 1 - gets the most common corresponding label from the actual labels
-from scipy.stats import mode
-labels = np.zeros_like(y_labels_train)
-for i in range(k):
-    mask = (y_labels_train == i)
-    labels[mask] = mode(y_train[mask])[0]
-
-# method 2 - returns a map of label to label and frequency (basically a check on method 1)
-label_map = {}
-for i in range(k):
-    label_counter = {'Class 0':0,'Class 1':0,'Class 2':0,'Class 3':0,'Class 4':0,'Class 5':0,'Class 6':0,'Class 7':0,'Class 8':0,'Class 9':0}
-    for idx in range(10128):
-        if y_labels_train[idx] == i:
-            item = y_train.values[idx]
-            if item == 0:
-                label_counter['Class 0'] += 1
-            elif item == 1:
-                label_counter['Class 1'] += 1
-            elif item == 2:
-                label_counter['Class 2'] += 1
-            elif item == 3:
-                label_counter['Class 3'] += 1
-            elif item == 4:
-                label_counter['Class 4'] += 1
-            elif item == 5:
-                label_counter['Class 5'] += 1
-            elif item == 6:
-                label_counter['Class 6'] += 1
-            elif item == 7:
-                label_counter['Class 7'] += 1
-            elif item == 8:
-                label_counter['Class 8'] += 1
-            elif item == 9:
-                label_counter['Class 9'] += 1
-            else:
-                print('ooopsy')
-    label_map[i] = label_counter
-
-
-# method 3 - ~ give dictionary but not frequency
-# def get_corresponding_labels(kmeans, dataset_labels):
-#     corrected_labels = {}
+# # set PCA for PCA accuracy
+# n_components = 784
+# whiten = False
+# random_state = 42
+# pca = PCA(n_components=n_components, whiten=whiten, random_state=random_state)
+# X_train_PCA = pca.fit_transform(X_train)
+# X_train_PCA = pd.DataFrame(data=X_train_PCA, index=train_index)
 #
-#     for i in range(kmeans.n_clusters):
-#         labels = []
-#         index = np.where(kmeans.labels_ == i)
-#         labels.append(dataset_labels.values[index])
+# # K-means - Accuracy as the number of components varies
+# accuracy_by_components = accuracy_by_num_components(X_train_PCA,y_train,n_clusters,n_init,max_iter,tol,random_state,n_jobs)
+# accuracy_by_components.plot().get_figure().savefig('overallAccuracyComponents.png')
 #
-#         if len(labels[0]) == 1:
-#             counts = np.bincount(labels[0])
-#         else:
-#             counts = np.bincount(np.squeeze(labels))
-#
-#         if np.argmax(counts) in corrected_labels:
-#             corrected_labels[np.argmax(counts)].append(i)
-#         else:
-#             corrected_labels[np.argmax(counts)] = [i]
-#
-#     return corrected_labels
-#
-# label_dictionary = get_corresponding_labels(kmeans, y_train)
+# # K-means using 100 PCA and X clusters
+# n_components = 100
+# whiten = False
+# random_state = 42
+# pca = PCA(n_components=n_components, whiten=whiten, random_state=random_state)
+# X_train_PCA = pca.fit_transform(X_train)
+# X_train_PCA = pd.DataFrame(data=X_train_PCA, index=train_index)
 
-# check labels
-labels
-y_labels_train
+n_clusters = 20
+n_init = 10
+max_iter = 300
+tol = 0.0001
+random_state = 42
+n_jobs = 2
+kmeans = KMeans(n_clusters=n_clusters, n_init=n_init, \
+            max_iter=max_iter, tol=tol, random_state=random_state, \
+            n_jobs=n_jobs)
 
-label_map
+# fit the model
+kmeans.fit_predict(X_train_PCA)
+get_label_mapping(kmeans, y_train)
